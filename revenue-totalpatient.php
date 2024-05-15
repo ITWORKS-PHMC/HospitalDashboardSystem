8f4b0a253d6ca056c11a7e69ef9842333039f0b6
Nistic18 <floydangelochee@gmail.com>
2024-05-15 18:01:12 +0800

    updateed totalrevenue

--- revenue-totalpatient.php
+++ revenue-totalpatient.php
@@ -4,40 +4,40 @@ require 'connection.php';
 $selected_year = isset($_GET['selected_year']) ? $_GET['selected_year'] : date('Y');
 $selected_month = isset($_GET['selected_month']) ? $_GET['selected_month'] : date('m');
 
-// SQL query to count total patient IDs for the selected year and month
-$sql = "SELECT COUNT(census_transaction_id) as totalcensus_id 
-        FROM dashboard_database 
-        WHERE YEAR(census_date_admitted) = $selected_year 
-        AND MONTH(census_date_admitted) = $selected_month";
+// SQL query to sum total revenue for the selected year and month
+$sql = "SELECT SUM(revenue_totalAmount) AS totalRevenue
+        FROM dashboard_revenue
+        WHERE YEAR(revenue_date) = $selected_year 
+        AND MONTH(revenue_date) = $selected_month";
 
 $result = $conn->query($sql);
 
 // Check if there are results
 if ($result->num_rows > 0) {
-    // Fetch the total patient count
+    // Fetch the total revenue
     $row = $result->fetch_assoc();
-    $totalPatients = $row["totalcensus_id"];
+    $totalRevenue = $row["totalRevenue"];
 
     // SQL query to get the target value for the selected year and month
-    $sql2 = "SELECT SUM(target_value) as totaltarget FROM dashboard_target";
+    $sql2 = "SELECT SUM(target_value) AS totalTarget FROM dashboard_target";
     $result2 = $conn->query($sql2);
-
+ 
     if ($result2->num_rows > 0) {
         // Fetch the target value
         $row2 = $result2->fetch_assoc();
-        $totalTarget = $row2["totaltarget"];
-        // Compare total patients with the target value
-        if ($totalPatients >= $totalTarget) {
-            // Display total patients with green arrow
-            echo "<div class='result' style='top:50px;left:100px;position: absolute; color:black;>$totalPatients<span class='green-arrow' style='float: right;'>&#8593;</span></div>";
+        $totalTarget = $row2["totalTarget"];
+        // Compare total revenue with the target value
+        if ($totalRevenue >= $totalTarget) {
+            // Display total revenue with green arrow
+            echo "<div class='result' style='top:50px;left:70px;position: absolute; color:black;'>$totalRevenue<span class='green-arrow' style='float: right;'>&#8593;</span></div>";
         } else {
-            // Display total patients with red arrow
-            echo "<div class='result' style='top:50px;left:100px;position: absolute; color:black;><span class='red-arrow' style='float: left;'>&#8595;</span>$totalPatients</div>";
+            // Display total revenue with red arrow
+            echo "<div class='result' style='top:50px;left:70px;position: absolute; color:black;'><span class='red-arrow' style='float: left;'>&#8595;</span>$totalRevenue</div>";
         }
     } else {
         echo "No target set for the selected year and month";
     }
 } else {
-    echo "No patients for the selected year and month";
+    echo "No revenue data for the selected year and month";
 }
 ?>
